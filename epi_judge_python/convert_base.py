e1c6f4c35c335aa5210121975a77ef88b60279da
Giuseppe <giuscri@gmail.com>
2020-04-05 18:34:25 +0200

    7.2

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
+M = "0123456789ABCDEF"
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    negative = num_as_string.startswith("-")
+    if negative:
+        num_as_string = num_as_string[1:]
+
+    n = 0
+    for digit in num_as_string:
+        n = n * b1 + int(digit, base=b1)
+
+    result = ""
+
+    while True:
+        result = M[n % b2] + result
+        n //= b2
+
+        if n < 1:
+            break
+
+    if negative:
+        result = "-" + result
+
+    return result
 
 
 if __name__ == '__main__':
