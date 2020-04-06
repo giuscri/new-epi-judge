2a17ac66f9b7f721ed48f013d2467ac8b4e8e5b5
Giuseppe <giuscri@gmail.com>
2020-04-06 23:55:33 +0200

    BONUS: 16.02

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,19 @@
+from functools import lru_cache
 from test_framework import generic_test
 
-
+@lru_cache()
 def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(A) == 0 or len(B) == 0:
+        return len(A) + len(B)
+
+    if A[0] == B[0]:
+        return levenshtein_distance(A[1:], B[1:])
+    
+    d = levenshtein_distance(A[1:], B)
+    s = levenshtein_distance(A[1:], B[1:])
+    i = levenshtein_distance(A, B[1:])
+
+    return 1 + min(d, s, i)
 
 
 if __name__ == '__main__':
