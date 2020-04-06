afaaefc5f090ac1d29cfffe2f59a9958ea5913c5
Giuseppe <giuscri@gmail.com>
2020-04-06 23:55:33 +0200

    BONUS: 16.02

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,19 +1,28 @@
 from functools import lru_cache
 from test_framework import generic_test
 
-@lru_cache()
 def levenshtein_distance(A: str, B: str) -> int:
-    if len(A) == 0 or len(B) == 0:
-        return len(A) + len(B)
+    n, m = len(A) + 1, len(B) + 1
+    dp = [[0 for _ in range(m)] for _ in range(n)]
 
-    if A[0] == B[0]:
-        return levenshtein_distance(A[1:], B[1:])
-    
-    d = levenshtein_distance(A[1:], B)
-    s = levenshtein_distance(A[1:], B[1:])
-    i = levenshtein_distance(A, B[1:])
+    for i in range(n):
+        dp[i][0] = i
+    for j in range(m):
+        dp[0][j] = j
 
-    return 1 + min(d, s, i)
+    for i in range(1, n):
+        for j in range(1, m):
+            # it's easier to understand why the definition of dp[i][j] is true
+            # moving from the bottom-right to the upper-left corner. For example:
+            # from dp[i][j] to dp[i][j - 1] is insert/append as we inserted/appended
+            # the last character of the second string to the first one and moved both forward
+            dp[i][j] = min(
+                1 + dp[i - 1][j], # delete
+                1 + dp[i][j - 1], # insert (append)
+                dp[i - 1][j - 1] + (1 if A[i - 1] != B[j - 1] else 0) # substitute or already same character
+            )
+
+    return dp[-1][-1]
 
 
 if __name__ == '__main__':
