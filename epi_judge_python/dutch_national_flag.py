1ef89c44b6722a121e78e91d61aa7f488b2b3cf2
Giuseppe <giuscri@gmail.com>
2020-04-13 00:02:28 +0200

    5.01

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -7,10 +7,39 @@ from test_framework.test_utils import enable_executor_hook
 
 RED, WHITE, BLUE = range(3)
 
+def _partition(A, start, stop, discriminator):
+  i, j = start, stop-1
+  while i <= j:
+    while i < stop and discriminator(A[i]):
+      i += 1
+    while j >= start and not discriminator(A[j]):
+      j -= 1
+
+    if j <= i:
+      break
+    else:
+      A[i], A[j] = A[j], A[i]
+      i += 1
+      j -= 1
+
+  return A[:i], A[i:], i
+
+def partition1(A, i, j, pivot):
+  """Partitions A[i:j] into < pivot, >= pivot"""
+
+  discriminator = lambda x, pivot=pivot: x < pivot
+  return _partition(A, i, j, discriminator)
+
+def partition2(A, i, j, pivot):
+  """Partitions A[i:j] into == pivot, > pivot"""
+
+  discriminator = lambda x, pivot=pivot: x == pivot
+  return _partition(A, i, j, discriminator)
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+  pivot = A[pivot_index]
+  _, _, i = partition1(A, 0, len(A), pivot)
+  _, _, _ = partition2(A, i, len(A), pivot)
 
 
 @enable_executor_hook
