a3c52db4942c3ca923a4cff8d7e8e42601bf4219
Giuseppe <giuscri@gmail.com>
2020-04-13 19:06:48 +0200

    6.18

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -4,8 +4,40 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if not square_matrix:
+        return []
+
+    r = []
+    n, m = len(square_matrix), len(square_matrix[0])
+    i, j, offset = 0, 0, 0
+    while offset < (n+1) // 2:
+        while j < m-offset:
+            r.append(square_matrix[i][j])
+            j += 1
+        j -= 1
+        i += 1
+
+        while i < n-offset:
+            r.append(square_matrix[i][j])
+            i += 1
+        i -= 1
+        j -= 1
+
+        while j >= offset:
+            r.append(square_matrix[i][j])
+            j -= 1
+        j += 1
+        i -= 1
+
+        while i >= offset+1:
+            r.append(square_matrix[i][j])
+            i -= 1
+        i += 1
+        j += 1
+
+        offset += 1
+
+    return r
 
 
 if __name__ == '__main__':
