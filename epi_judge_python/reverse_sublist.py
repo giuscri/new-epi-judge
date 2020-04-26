8ee40d766f624770f37ebfa188d858b6f4c0dbe0
Giuseppe <giuscri@gmail.com>
2020-04-26 19:58:36 +0200

    8.3
    use iterations instead of recursion

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -2,12 +2,43 @@ from typing import Optional
 
 from list_node import ListNode
 from test_framework import generic_test
+from math import inf
 
 
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L is None or L.next is None:
+        return L
+
+    # set `p` to where the sublist to reverse starts
+    # prev_p is the last node of the original list head
+    prev_p = None
+    p = L
+    i = 1
+    while p is not None and i < start:
+        prev_p = p
+        p = p.next
+        i += 1
+
+    # reverse the sublist starting at p
+    previous = None
+    current = p
+    while current is not None and i <= finish:
+        current_next = current.next
+        current.next = previous
+        previous = current
+        current = current_next
+        i += 1
+
+    # attach sublist to the original list tail
+    p.next = current
+
+    # attach original list head to sublist
+    if prev_p is not None:
+        prev_p.next = previous
+        return L # the head hasn't changed
+    else:
+        return previous
 
 
 if __name__ == '__main__':
