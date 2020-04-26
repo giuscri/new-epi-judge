d01eaaa1ddb1bfa52fadf8dcfa5c68364bf1fde4
Giuseppe <giuscri@gmail.com>
2020-04-26 18:55:50 +0200

    7.7

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,30 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+M = {
+    "0": "0",
+    "1": "1",
+    "2": "ABC",
+    "3": "DEF",
+    "4": "GHI",
+    "5": "JKL",
+    "6": "MNO",
+    "7": "PQRS",
+    "8": "TUV",
+    "9": "WXYZ"
+}
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    if not phone_number:
+        return [""]
+
+    n = phone_number[0]
+    l = phone_mnemonic(phone_number[1:])
+    ll = []
+    for c in M[n]:
+        for s in l:
+            ll.append(c + s)
+    return ll
 
 
 if __name__ == '__main__':
