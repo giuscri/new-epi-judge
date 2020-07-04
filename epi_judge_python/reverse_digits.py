dcca1f0b70c4afd37cabdebaa70beca9499c3ec1
Giuseppe <giuscri@gmail.com>
2020-07-04 16:22:58 +0200

    reverse digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,9 +2,20 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    negative_input = x < 0
+    if negative_input:
+        x *= -1
 
+    r = 0
+    while x > 0:
+        r *= 10
+        r += x % 10
+        x //= 10
+
+    if negative_input:
+        r *= -1
+
+    return r
 
 if __name__ == '__main__':
     exit(
