e47b24198eb6a5c671cb0069883bc6238b4195a3
Giuseppe <giuscri@gmail.com>
2020-07-04 17:20:12 +0200

    valid bst

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,10 +1,28 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from typing import Tuple
+
+def f(tree: BinaryTreeNode) -> Tuple[bool, int, int]:
+    if tree.left is None:
+        left_valid, mL, ML = True, tree.data, tree.data
+    else:
+        left_valid, mL, ML = f(tree.left)
+
+    if tree.right is None:
+        right_valid, mR, MR = True, tree.data, tree.data
+    else:
+        right_valid, mR, MR = f(tree.right)
+
+    valid = left_valid and right_valid and ML <= tree.data <= mR
+    return (valid, mL, MR)
 
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if tree is None:
+        return True
+
+    valid, _, _ = f(tree)
+    return valid
 
 
 if __name__ == '__main__':
