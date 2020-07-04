e8e949a98f4745d266c0657caabb85adeb4fcf0f
Giuseppe <giuscri@gmail.com>
2020-07-04 18:03:48 +0200

    range lookup but brute-force

--- epi_judge_python/range_lookup_in_bst.py
+++ epi_judge_python/range_lookup_in_bst.py
@@ -6,10 +6,21 @@ from test_framework import generic_test
 
 Interval = collections.namedtuple('Interval', ('left', 'right'))
 
+def inorder(tree: BstNode, r: List[int], interval: Interval) -> None:
+    if tree is None:
+        return
+
+    inorder(tree.left, r, interval)
+
+    if interval.left <= tree.data <= interval.right:
+        r.append(tree.data)
+
+    inorder(tree.right, r, interval)
 
 def range_lookup_in_bst(tree: BstNode, interval: Interval) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    r = []
+    inorder(tree, r, interval)
+    return r
 
 
 def range_lookup_in_bst_wrapper(tree, i):
