67fe37ea84e8d41ee8a722ea8256dabbab92c39f
Giuseppe <giuscri@gmail.com>
2020-07-04 16:35:32 +0200

    classic rpn problem

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack = []
+
+    for token in expression.split(","):
+        if token in "+-*":
+            b, a = stack.pop(), stack.pop()
+            stack.append(eval(f"{a} {token} {b}"))
+        elif token == "/":
+            b, a = stack.pop(), stack.pop()
+            stack.append(eval(f"{a} // {b}"))
+        else:
+            stack.append(token)
+
+    return int(stack.pop())
 
 
 if __name__ == '__main__':
