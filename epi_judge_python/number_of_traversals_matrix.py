69763acf1b12795174d37547cb29b1c49c721dfd
Giuseppe <giuscri@gmail.com>
2020-07-05 16:19:23 +0200

    number of ways to traverse matrix

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    prev = [1 for _ in range(m)]
+    row = [1 for _ in range(m)]
+
+    for _ in range(1, n):
+        for j in range(1, m):
+            row[j] = prev[j] + row[j-1]
+        prev = row
+
+    return row[-1]
 
 
 if __name__ == '__main__':
