0a6b0a9d987c5f06edda4eafad3a68265aeb3a85
Giuseppe <giuscri@gmail.com>
2020-07-11 16:19:35 +0200

    check if balanced tree

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,11 +1,19 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
+from typing import Tuple
 
+def f(tree: BinaryTreeNode) -> Tuple[bool, int]:
+    if tree is None:
+        return (True, -1)
+
+    left_balanced, left_height = f(tree.left)
+    right_balanced, right_height = f(tree.right)
+    balanced = left_balanced and right_balanced and abs(left_height - right_height) <= 1
+    return (balanced, max(left_height, right_height) + 1)
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
-
+    balanced, _ = f(tree)
+    return balanced
 
 if __name__ == '__main__':
     exit(
