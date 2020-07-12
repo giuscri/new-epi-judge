1af2a6eaa8c5ac1127787c74ae8a056c028f2bd4
Giuseppe <giuscri@gmail.com>
2020-07-12 16:01:57 +0200

    merge in place

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,7 +5,40 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    if len(B) != n:
+        raise RuntimeError("n should equal len(B)")
+
+    if len(A) < len(B):
+        raise RuntimeError("A should be larger than B")
+
+    i = 0
+    while i < m:
+        A[len(A)-1-i] = A[m-1-i]
+        i += 1
+
+    i = len(A)-m
+    j = 0
+    k = 0
+
+    while i < len(A) and j < len(B):
+        if A[i] < B[j]:
+            A[k] = A[i]
+            i += 1
+        else:
+            A[k] = B[j]
+            j += 1
+        k += 1
+
+    while i < len(A):
+        A[k] = A[i]
+        i += 1
+        k += 1
+
+    while j < len(B):
+        A[k] = B[j]
+        j += 1
+        k += 1
+
     return
 
 
