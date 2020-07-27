224bacda6fc675d43368d63f92e6c99f80b79ec1
Giuseppe <giuscri@gmail.com>
2020-07-27 11:11:37 +0200

    find valid ip addresses

--- epi_judge_python/valid_ip_addresses.py
+++ epi_judge_python/valid_ip_addresses.py
@@ -1,11 +1,29 @@
-from typing import List
+from typing import List, Optional
 
 from test_framework import generic_test
 
+def f(s: str, i: int, n: int, prefix: str) -> Optional[List[str]]:
+    assert n >= 0
+
+    if i >= len(s) and n > 0:
+        return None
+    elif i < len(s) and n == 0:
+        return None
+    elif i >= len(s) and n == 0:
+        return [prefix]
+
+    r = []
+    r.extend(f(s, i+1, n-1, f"{prefix}.{s[i]}") or [])
+    if i+1 < len(s) and s[i] != "0" and int(s[i:i+2]) > 0:
+        r.extend(f(s, i+2, n-1, f"{prefix}.{s[i:i+2]}") or [])
+    if i+2 < len(s) and s[i] != "0" and s[i:i+2] != "00" and 1 < int(s[i:i+3]) < 256:
+        r.extend(f(s, i+3, n-1, f"{prefix}.{s[i:i+3]}") or [])
+
+    return r
 
 def get_valid_ip_address(s: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    ips = f(s, 0, 4, "") or []
+    return list(map(lambda ip: ip[1:], ips)) # strip initial dot
 
 
 def comp(a, b):
