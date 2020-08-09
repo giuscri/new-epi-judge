6b74414e0ffd51fbd7d394ef9f46282135df167e
Giuseppe <giuscri@gmail.com>
2020-08-09 17:07:05 +0200

    max trapped water

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -1,11 +1,25 @@
 from typing import List
+import sys
 
 from test_framework import generic_test
 
+INF = sys.maxsize
 
 def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    i, j = 0, len(heights)-1
+    m = -INF
+    while i < j:
+        if heights[i] < heights[j]:
+            m = max(m, abs(i-j) * heights[i])
+            i += 1
+        elif heights[j] < heights[i]:
+            m = max(m, abs(i-j) * heights[j])
+            j -= 1
+        else: # height[i] == height[j]
+            m = max(m, abs(i-j) * heights[j])
+            i += 1
+            j -= 1
+    return m
 
 
 if __name__ == '__main__':
