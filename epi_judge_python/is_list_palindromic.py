d63553cc84ed8546600a50171f2064f8bf849d19
Giuseppe <giuscri@gmail.com>
2020-10-04 11:14:49 +0200

    8.12

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -1,9 +1,54 @@
-from list_node import ListNode
+from list_node import ListNode, list_size
 from test_framework import generic_test
+from typing import List
 
+def reverse(head: ListNode) -> ListNode:
+    if head is None or head.next is None:
+        return head
+
+    prev, curr = None, head
+    while curr is not None:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+
+    return prev
 
 def is_linked_list_a_palindrome(L: ListNode) -> bool:
-    # TODO - you fill in here.
+    if not L:
+        return True
+
+    length = list_size(L)
+    if length%2 == 0:
+        mid = (length // 2) - 1
+    else:
+        mid = length // 2
+
+
+    p = L
+    i = 0
+    while p is not None and i < mid:
+        p = p.next
+        i += 1
+
+    p.next = reverse(p.next)
+
+    p = L
+    i = 0
+    while p is not None and i <= mid:
+        p = p.next
+        i += 1
+
+    q = L
+    i = 0
+    while p is not None and i < mid:
+        if q.data != p.data:
+            return False
+
+        p = p.next
+        q = q.next
+
     return True
 
 
