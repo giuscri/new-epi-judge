8b7a5e74a3acb413d02d8a574c5a34d8bc6566af
Giuseppe <giuscri@gmail.com>
2020-10-19 18:57:38 +0200

    find max subarray

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -4,8 +4,22 @@ from test_framework import generic_test
 
 
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+    n = len(A)
+    S = 0
+    min_sum = 0
+    max_sum = 0
+    a, b = -1, -1
+    min_idx = -1
+    for j in range(n):
+        S += A[j]
+        if S < min_sum:
+            min_sum = S
+            min_idx = j
+        if S - min_sum > max_sum:
+            max_sum = S - min_sum
+            a, b = min_idx + 1, j
+    assert max_sum == sum(A[a:b+1])
+    return max_sum
 
 
 if __name__ == '__main__':
