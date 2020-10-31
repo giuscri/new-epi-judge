321f2d271c63cafe5edf53623873b35636ceca69
Giuseppe <giuscri@gmail.com>
2020-10-31 18:28:42 +0100

    number of score combinations

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,8 +5,12 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    dp = [0 for _ in range(final_score+1)]
+    dp[0] = 1
+    for ps in individual_play_scores:
+        for j in range(ps, final_score+1):
+            dp[j] += dp[j-ps]
+    return dp[final_score]
 
 
 if __name__ == '__main__':
