749731e6b72be3986251525912605c864e9706f4
Giuseppe <giuscri@gmail.com>
2020-11-06 18:40:43 +0100

    longest non-decreasing subsequence

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -4,8 +4,18 @@ from test_framework import generic_test
 
 
 def longest_nondecreasing_subsequence_length(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    n = len(A)
+    dp = [0 for _ in range(n)]
+    for i in range(n):
+        M = 0
+        for j in range(i):
+            if A[j] > A[i]:
+                continue
+            M = max(M, dp[j])
+
+        dp[i] = M+1
+
+    return max(dp)
 
 
 if __name__ == '__main__':
